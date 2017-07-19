fc1817fbec22777b4f3eddb8f7398ca6e1b729c2
Admin User <admin@guardianfocus.com>
2017-07-18 21:20:42 -0700

    beautify

--- webapp/app.js
+++ webapp/app.js
@@ -6,18 +6,20 @@ var port = 5000;
 
 app.use(express.static('public'));
 
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/',function(req, res){
-	res.render('index',{title: 'hello from ...'});
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'hello from ...'
+    });
 });
 
-app.get('/download', function(req, res){
-	res.send('Hello Download');
+app.get('/download', function (req, res) {
+    res.send('Hello Download');
 });
 
-app.listen(5000, function(err){
+app.listen(5000, function (err) {
 
-	console.log('running server on port '+ port);
+    console.log('running server on port ' + port);
 });
